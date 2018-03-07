b0b616ddad8b3fe7900ae8cc93c084aec52712dd
Pierre Dargham <pierre.dargham@globalis-ms.com>
2018-03-07 16:23:39 +0100

    Change plugin version tag to 1.0.0

--- wp-cubi-imagemin.php
+++ wp-cubi-imagemin.php
@@ -8,9 +8,9 @@
  * Author URI:          https://www.globalis-ms.com/
  * License:             GPL2
  *
- * Version:             0.1.0
+ * Version:             1.0.0
  * Requires at least:   4.6.0
- * Tested up to:        4.9.2
+ * Tested up to:        4.9.4
  */
 
 namespace Globalis\WP\Cubi\ImageMin;
